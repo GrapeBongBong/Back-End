f6a7595fe052d528fae41a63d7c4ac8de164c0a6
장정윤 <jeinie9179@naver.com>
2023-05-14 00:57:51 +0900

    fix: region 설정

--- src/main/java/com/example/capstone/config/S3Config.java
+++ src/main/java/com/example/capstone/config/S3Config.java
@@ -3,6 +3,7 @@ package com.example.capstone.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,7 +28,7 @@ public class S3Config {
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+                .withRegion(Regions.AP_NORTHEAST_2)
                 .build();
     }
 }
