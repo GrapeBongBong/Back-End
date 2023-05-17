22a8acd5b8518111d68487927d571feb8d5f0909
장정윤 <jeinie9179@naver.com>
2023-05-17 23:01:58 +0900

    fix: 게시글 기준으로 채팅방 조회

--- src/main/java/com/example/capstone/repository/ChatRoomRepository.java
+++ src/main/java/com/example/capstone/repository/ChatRoomRepository.java
@@ -1,13 +1,15 @@
 package com.example.capstone.repository;
 
 import com.example.capstone.entity.ChatRoom;
+import com.example.capstone.entity.ExchangePost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    // 채팅방 조회
-    List<ChatRoom> findAll();
-
+    // 채팅방 조회 (roomId 기준)
     ChatRoom findChatRoomByRoomId(Long roomId);
+
+    // 채팅방 조회 (게시글 기준)
+    List<ChatRoom> findChatRoomsByExchangePost(ExchangePost exchangePost);
 }
