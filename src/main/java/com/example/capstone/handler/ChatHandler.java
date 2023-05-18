0120f130caaee217d8466069d662910313d37a35
장정윤 <jeinie9179@naver.com>
2023-05-18 23:45:25 +0900

    fix: 채팅 메시지 응답 내용 수정

--- src/main/java/com/example/capstone/handler/ChatHandler.java
+++ src/main/java/com/example/capstone/handler/ChatHandler.java
@@ -50,7 +50,7 @@ public class ChatHandler extends TextWebSocketHandler { // Client 가 Send 할 �
         // 해당 채팅방의 세션들에게 메시지 전송
         List<WebSocketSession> roomSessions = chatSessions.getOrDefault(chatRoomId, new ArrayList<>());
         for (WebSocketSession roomSession: roomSessions) {
-            roomSession.sendMessage(new TextMessage(chatMessageDTO.getMessage()));
+            roomSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(chatMessageDTO)));
         }
     }
 
