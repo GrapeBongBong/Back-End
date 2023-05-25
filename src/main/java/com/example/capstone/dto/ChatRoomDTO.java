ff2f2da412d601a971b245c52b052a1f8123c600
장정윤 <jeinie9179@naver.com>
2023-05-25 20:31:47 +0900

    fix: 채팅방 목록 조회 응답 내용 수정

--- src/main/java/com/example/capstone/dto/ChatRoomDTO.java
+++ src/main/java/com/example/capstone/dto/ChatRoomDTO.java
@@ -9,13 +9,12 @@ import java.util.List;
 import java.util.Objects;
 
 @Data
-@Getter
-@Setter
-@ToString
 public class ChatRoomDTO {
     // json 데이터를 받아 WebSocketHandler 에서 해당 데이터에 담긴 roomId 를 chatService 를 통해서 조회
     // 해당 id 의 채팅방을 찾아 json 데이터에 담긴 메시지를 해당 채팅방으로 보냄
 
+    private Long postWriterUID;
+    private Long pid;
     private Long roomId;
     private String roomName;
     private String date; // 채팅방 생성날짜
@@ -33,6 +32,8 @@ public class ChatRoomDTO {
 
     private static ChatRoomDTO toChatRoomDTO(ChatRoom chatRoom) {
         ChatRoomDTO chatRoomDTO = new ChatRoomDTO();
+        chatRoomDTO.setPostWriterUID(chatRoom.getPostWriter().getUid());
+        chatRoomDTO.setPid(chatRoom.getExchangePost().getPid());
         chatRoomDTO.setRoomId(chatRoom.getRoomId());
         chatRoomDTO.setRoomName(chatRoom.getRoomName());
         chatRoomDTO.setDate(chatRoom.getDate());
